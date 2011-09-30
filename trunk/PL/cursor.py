ba28d49030486e7f9019189f509bcad22bb9a09a
alberto.a.oliv@gmail.com <alberto.a.oliv@gmail.com@1931bacc-f327-3af6-eab4-29e089e53327>
2011-09-30 22:01:25 +0000

    Tweaks diversos no codigo, um tanto de bugs corrigidos. So esta precisando de uma faxina geral pra comentar coisas, e tirar algumas coisas inuteis. Tres botoes adicionados para teste. r e o mais importante, ele gera diversos movimentos aleatorios em um espaco curto de tempo pra testar se todas as animacoes funcionam certinho.

--- trunk/PL/cursor.py
+++ trunk/PL/cursor.py
@@ -22,32 +22,48 @@ class Choice_cursor(pygame.sprite.Sprite):
 	self.rect = self.image.get_rect()
 	self.rect.topleft = (surf_rect.left+10 + self.cs_w*2 ,surf_rect.top+5 + self.cs_h)
 	
+	# limite superior que o bloco nao pode passar
+	self.top_limit = surf_rect.top + 5
+	
+	self.update_counter = 0
+	
 	# Posicoes relativas do cursor em relacao a matriz de blocos
 	self.pos_rel_x = 2
 	self.pos_rel_y = 10
 
+    def update(self, rise_value):
+        if self.rect.top - rise_value <= self.top_limit:
+            self.rect.top += self.cs_h
+            
+        self.rect.top -= rise_value
+        self.update_counter += rise_value
+        if self.update_counter == 21:
+            self.update_counter = 0
+            self.pos_rel_y += 1
 
     # Desenha o cursor na tela. Borda de retangulo branca
     def draw_cursor(self, surf):
 	surf.blit(self.image, self.rect)
 
     # Controla o movimento do cursor
-    def move_cursor_UP(self, key, rect):    
-	if self.rect.top - self.cs_h >= rect.top + 5:
+    def move_cursor_UP(self, key, surf_rect):    
+	if self.rect.top - self.cs_h >= self.top_limit:
 	    self.rect.top -= self.cs_h
 	    self.pos_rel_y += 1
 	    
-    def move_cursor_DOWN(self, key, rect):  	    
-	if self.rect.top + self.cs_h <= rect.bottom - 20:
+    def move_cursor_DOWN(self, key, surf_rect):  	    
+	if self.rect.top + self.cs_h <= surf_rect.bottom - 20:
 	    self.rect.top += self.cs_h
 	    self.pos_rel_y -= 1
 	    
-    def move_cursor_LEFT(self, key, rect):  	
-	if self.rect.left - self.cs_w >= rect.left:
+    def move_cursor_LEFT(self, key, surf_rect):  	
+	if self.rect.left - self.cs_w >= surf_rect.left:
 	    self.rect.left -= self.cs_w
 	    self.pos_rel_x -= 1
 	    
-    def move_cursor_RIGHT(self, key, rect):  	    
-	if self.rect.right + self.cs_w <= rect.right:
+    def move_cursor_RIGHT(self, key, surf_rect):  	    
+	if self.rect.right + self.cs_w <= surf_rect.right:
 	    self.rect.left += self.cs_w
 	    self.pos_rel_x += 1
+	    
+    
