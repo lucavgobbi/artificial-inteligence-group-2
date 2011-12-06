05fbdc2e97ea544def7f1f243302ff38f3e27d6d
alberto.a.oliv@gmail.com <alberto.a.oliv@gmail.com@1931bacc-f327-3af6-eab4-29e089e53327>
2011-12-06 20:24:49 +0000

    Comentarios

--- PL/cpu.py
+++ PL/cpu.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import os, sys
 import pygame
@@ -15,32 +16,43 @@ import cPickle
 class Cpu:
     
     def __init__(self, pos_size, surf, ini_max_height):
-        
+
+        # Blockbox da CPU
         self.blockbox = Blockbox(pos_size[0], pos_size[1], pos_size[2], pos_size[3], surf, True, ini_max_height)
-        
-        self.raw_move_queue = []        
+
+        # Fila de pares de coordenadas de blocos que devem ser trocados com seus vizinhos
+        self.raw_move_queue = []
+
+        # Fila de movimentos que devem ser executados
         self.t_move_queue = []
-        
+
+        # Posicao que o cursor termina apos um movimento. Inicial e [2,10]
         self.cursor_final_position = [2,10]
-        
-        self.max_move_timer = 5
+
+        # Controla o tempo de reacao da CPU. Quanto menor, mais rapido ela executa os movimentos
+        self.max_move_timer = 3
         self.move_timer = self.max_move_timer
-        
+
+        # Thread em que a IA executa
         self.ia = None
-        
+
+        # Usada para parar de chamar a IA quando necessario
         self.stop_ia = False
-        
+
+        # Ultima matriz expandida
         self.last_m = []
-        
+
+        # Sinaliza a necessidade de subir uma linha, quando nao ha movimentos para a matriz corrente
         self.need_line = False
-        
+
+        # Arquivo que salva bons movimentos conhecidos
         if(os.path.isfile("know.moves")):
             f = open("know.moves", 'r')
             self.knowMoves = cPickle.load(f)
         else:
             self.knowMoves = dict()
 
-#forca uma nova linha a surgir
+    #forca uma nova linha a surgir
     def rise_line(self):
         self.blockbox.block_group.update(3)
         self.blockbox.cursor_group.update(3)
@@ -55,13 +67,13 @@ class Cpu:
             
 #self.init_ia()
       
-#salva o arquivo de KnowMoves
+    #salva o arquivo de KnowMoves
     def saveKnowMoves(self):
         f = open('know.moves', 'w')
         cPickle.dump(self.knowMoves, f)
         f.close()
 
-#chama ia sem thread
+    #chama ia sem thread
     def call_ia(self):
         tree = buildTree(self.last_m, 3)
         best = maxPath(tree)
@@ -78,7 +90,7 @@ class Cpu:
         self.raw_move_queue.append(l)
         self.transform_movements()
 
-#inicia a ia com thread
+    #inicia a ia com thread
     def init_ia(self):
         #printMatrix(self.blockbox.block_config)
         self.ia = IaThread(self.blockbox.block_config, self.knowMoves)
@@ -87,7 +99,7 @@ class Cpu:
         self.t_move_queue = []
         self.stop_ia = False
 
-#looping da ia com thread
+    #looping da ia com thread
     def call_ia2(self):
         l = []
         
@@ -97,11 +109,12 @@ class Cpu:
             if self.ia.isAlive():
                 return
             else:
+                # se a IA tiver devolvidos
                 if len(self.ia.path) > 0:
                     for move in self.ia.path:
                         l.append([move.c, move.r])
-                        #print move.c, move.r
-                    
+
+                    # Se a matriz do ultimo movimento retornado for diferente de nula, relança a IA
                     end_matrix = copy.deepcopy(self.ia.path[-1].m)
                     if end_matrix != []:
                         self.ia = None
@@ -115,9 +128,8 @@ class Cpu:
                 else:
                     self.stop_ia = True;
                     self.need_line = True
-                #self.stop_ia = True
 
-#gera movimentos aleatorios
+    # gera movimentos aleatorios
     def gen_random_movements(self):
         
         if len(self.t_move_queue) < 8:
@@ -145,6 +157,8 @@ class Cpu:
             
             if moves != []: self.raw_move_queue.append(moves)
 
+    # transformar movimentos de um formato de uma lista de pares de coordenadas de blocos que devem ser trocados,
+    # para uma codificacao que permite que a IA execute os movimentos correspondentes
     def transform_movements(self):
         moves_to_transform = self.raw_move_queue.pop(0)
         size = len(moves_to_transform)
@@ -201,7 +215,7 @@ class Cpu:
         self.t_move_queue.append(move_list)
         self.cursor_final_position = moves_to_transform[-1]
         
-#executa os movimentos que estao na fila
+    #executa os movimentos transformados que foram enfileirados
     def execute_cpu_movements(self):
         
         if self.t_move_queue == []:
--- PL/msg.py
+++ PL/msg.py
@@ -3,7 +3,7 @@
 import pygame
 from adds import *
 
-# Representa o score na tela
+# Representa mensagens que apareçam na tela
 class MSG(pygame.sprite.Sprite):
     # Inicializacao
     def __init__(self, surf_rect, text, size, (pos_x, pos_y), color):
@@ -11,20 +11,18 @@ class MSG(pygame.sprite.Sprite):
         # Inicializa classe sprite
         pygame.sprite.Sprite.__init__(self)
 
-        # Valor de pontos
-        self.value = 0
-
         # Fonte a ser usada
         self.font = pygame.font.Font(None, size)
 
+        # Surface aonde a mensagem deve aparecer
         self.surf_rect = surf_rect
 
         # Chamada para renderizacao inicial do score
-        self.render_score(text, (pos_x, pos_y), color)
+        self.render_msg(text, (pos_x, pos_y), color)
 
-    ## Atualiza o score em uma nova surface para refletir pontos feitos pelo jogador
+    ## Atualiza a mensagem em uma nova surface
     #  @param score_value: valor a a ser adicionado ao score
-    def render_score(self, text, (pos_x, pos_y), color):
+    def render_msg(self, text, (pos_x, pos_y), color):
 
         self.image = self.font.render(text, 1, color)
         self.rect = self.image.get_rect()
