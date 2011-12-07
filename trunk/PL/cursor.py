282ab570d1f7f15066cf122601eb5b5c25a9de7e
alberto.a.oliv@gmail.com <alberto.a.oliv@gmail.com@1931bacc-f327-3af6-eab4-29e089e53327>
2011-12-07 02:52:44 +0000

    Commit final. Comentarios apenas

--- trunk/PL/cursor.py
+++ trunk/PL/cursor.py
@@ -1,11 +1,22 @@
 #! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import pygame
 from adds import *
 
-# Representa o cursor movimentado pelo jogador
+
 class Choice_cursor(pygame.sprite.Sprite):
-    # Inicializacao
+    """
+    # Representa o cursor de troca movimentado pelo jogador
+    """
+    
     def __init__(self, surf_rect, w, h):
+        """
+        ## Construtor
+        # @param surf_rect: superficie de orientação para o cursor (no caso, a blockbox)
+        # @param w: largura do cursor
+        # @param h: altura do cursor
+        """
     
         # Inicializa classe sprite
         pygame.sprite.Sprite.__init__(self)
@@ -26,51 +37,85 @@ class Choice_cursor(pygame.sprite.Sprite):
     
         # limite superior que o cursor nao pode passar
         self.top_limit = surf_rect.top + 5
-    
+        
+        # Contador para controle de atualização de tela e portanto reposicionamento do cursor
         self.update_counter = 0
     
         # Posicoes relativas do cursor em relacao a matriz de blocos
         self.pos_rel_x = 2
         self.pos_rel_y = 10
 
-    # Atualiza a posicao do cursor
+    
     def update(self, rise_value):
+        """
+        ## Atualiza a posicao do cursor, fazendo com que ele suba uma quantidade de pixels dada
+        # @param rise_value: numero de pixels queo cursor deve subir
+        """
+        
+        # se o topo do cursor ultrapassar o limite da tela, ao inves de fazelo subir, joga ele
+        # uma posição para baixo
         if self.rect.top - rise_value <= self.top_limit:
             self.rect.top += self.cs_h
             self.pos_rel_y -= 1
-            
+        
+        # sobe o cursor o número de pixels dado, e caos ultrapasse 21, aumenta sua posição relativa
+        # e reseta o contador
         self.rect.top -= rise_value
         self.update_counter += rise_value
         if self.update_counter == 21:
             self.update_counter = 0
             self.pos_rel_y += 1
 
-    # Desenha o cursor na tela. Borda de retangulo branca
+
     def draw_cursor(self, surf):
+        """
+        ## Desenha o cursor na superficie dada
+        # @param surf: superficie em que deve ser desenhado o cursor
+        """
+        
         surf.blit(self.image, self.rect)
 
     def reset_cursor(self):
+        """
+        ## Reseta o cursor para a posição inicial coluna 2 e linha 10
+        """
         self.rect.topleft = (self.surf_rect.left+10 + self.cs_w*2 ,self.surf_rect.top+5 + self.cs_h)
         self.pos_rel_x = 2
         self.pos_rel_y = 10
 
-    # Controla o movimento do cursor
-    def move_cursor_UP(self, surf_rect):    
+
+    def move_cursor_UP(self, surf_rect):
+        """
+        ## Movimenta cursor para cima de acordo com a superficie a qual ele se orienta
+        #  @param surf_rect: superficie que orienta o cursor
+        """
         if self.rect.top - self.cs_h >= self.top_limit:
             self.rect.top -= self.cs_h
             self.pos_rel_y += 1
         
-    def move_cursor_DOWN(self, surf_rect):    
+    def move_cursor_DOWN(self, surf_rect): 
+        """
+        ## Movimenta cursor para baixo de acordo com a superficie a qual ele se orienta
+        #  @param surf_rect: superficie que orienta o cursor
+        """
         if self.rect.top + self.cs_h <= surf_rect.bottom - 20:
             self.rect.top += self.cs_h
             self.pos_rel_y -= 1
         
     def move_cursor_LEFT(self, surf_rect):
+        """
+        ## Movimenta cursor para esquerda de acordo com a superficie a qual ele se orienta
+        #  @param surf_rect: superficie que orienta o cursor
+        """
         if self.rect.left - self.cs_w >= surf_rect.left:
             self.rect.left -= self.cs_w
             self.pos_rel_x -= 1
         
-    def move_cursor_RIGHT(self, surf_rect):   
+    def move_cursor_RIGHT(self, surf_rect):
+        """
+        ## Movimenta cursor para direita de acordo com a superficie a qual ele se orienta
+        #  @param surf_rect: superficie que orienta o cursor
+        """
         if self.rect.right + self.cs_w <= surf_rect.right:
             self.rect.left += self.cs_w
             self.pos_rel_x += 1
--- trunk/PL/msg.py
+++ trunk/PL/msg.py
@@ -3,26 +3,37 @@
 import pygame
 from adds import *
 
-# Representa mensagens que apareçam na tela
 class MSG(pygame.sprite.Sprite):
-    # Inicializacao
-    def __init__(self, surf_rect, text, size, (pos_x, pos_y), color):
+    """
+    # Cria e renderiza menagens para aparecer na tela
+    """
+    
+    def __init__(self, text, size, (pos_x, pos_y), color):
+        """
+        ## Construtor
+        #  @param surf_rect: rect da superficie base para o score
+        #  @param text: texto a ser mostrado
+        #  @param size: tamanho da fonte
+        #  @param (pos_x, pos_y): tupla com a coordenada onde será posicionada o texto (canto superior esquerdo)
+        #  @param color: cor do texto
+        """
 
         # Inicializa classe sprite
         pygame.sprite.Sprite.__init__(self)
 
-        # Fonte a ser usada
+        # Inicializa fonte a ser usada
         self.font = pygame.font.Font(None, size)
 
-        # Surface aonde a mensagem deve aparecer
-        self.surf_rect = surf_rect
-
-        # Chamada para renderizacao inicial do score
+        # Chamada para renderizacao inicial da mensagem
         self.render_msg(text, (pos_x, pos_y), color)
 
-    ## Atualiza a mensagem em uma nova surface
-    #  @param score_value: valor a a ser adicionado ao score
     def render_msg(self, text, (pos_x, pos_y), color):
+        """
+        ## Renderiza a mensagem, criando seu rect para ser grudada na tela
+        #  @param text: texto a ser mostrado
+        #  @param (pos_x, pos_y): tupla com a coordenada onde será posicionada o texto (canto superior esquerdo)
+        #  @param color: cor do texto
+        """
 
         self.image = self.font.render(text, 1, color)
         self.rect = self.image.get_rect()
